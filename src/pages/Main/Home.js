c0627e56107ff0171630febc4a635d811c9d71ab
Al Amin Sheikh <azalamins@gmail.com>
2023-08-30 13:48:31 +0600

    redux logger and middleware added

--- src/pages/Main/Home.js
+++ src/pages/Main/Home.js
@@ -11,7 +11,7 @@ const Home = () => {
   // const filters = useSelector(state => state.filter.filters);
   // const {brands, stock} = filters
 
-  const {brands, stock} = useSelector(state => state.filter.filters);
+  const { brands, stock } = useSelector(state => state.filter.filters);
 
   useEffect(() => {
     // Live products
@@ -34,7 +34,7 @@ const Home = () => {
 
   if (products.length && (stock || brands.length)) {
     content = products.filter((product) => {
-      if(stock){
+      if (stock) {
         return product.status === true
       }
       return product
@@ -48,12 +48,10 @@ const Home = () => {
     ))
   }
 
-  console.log(products)
-
   return (
     <div className='max-w-7xl gap-14 mx-auto my-10'>
       <div className='mb-10 flex justify-end gap-5'>
-        <button onClick={()=> dispatch(toggleStock())}
+        <button onClick={() => dispatch(toggleStock())}
           className={`border px-3 py-2 rounded-full font-semibold ${stock ? activeClass : null} `}
         >
           In Stock
